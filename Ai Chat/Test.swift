453060c5c9617188d34af728e134c56c7b3ed2c7
steve spencer <sspencer10@gmail.com>
2024-11-23 02:35:58 -0600

    11/22

---
+++ Ai Chat/Test.swift
@@ -0,0 +1,8 @@
+//
+//  Test.swift
+//  Chat with Ai Plus
+//
+//  Created by Steven Spencer on 11/19/24.
+//
+
+import Foundation
---
+++ Ai Chat/Webview.swift
@@ -0,0 +1,8 @@
+//
+//  Webview.swift
+//  Chat with Ai Plus
+//
+//  Created by Steven Spencer on 11/22/24.
+//
+
+import Foundation
